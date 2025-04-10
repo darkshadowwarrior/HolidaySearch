5bffa7acd6b63fcd13427b7fa2e6f9fd9a795b7c
Ian Richards <cdeveloper@hotmail.co.uk>
2025-04-10 22:39:51 +0100

    Missed out flight service

---
+++ HolidaySearch/Services/FlightService.cs
@@ -0,0 +1,20 @@
+﻿using HolidaySearch.Interfaces;
+using HolidaySearch.Models;
+
+namespace HolidaySearch.Services
+{
+    public class FlightService : IFlightService
+    {
+        private IFlightData _flightData;
+
+        public FlightService(IFlightData flightData)
+        {
+            _flightData = flightData;
+        }
+
+        public List<Flight> FilterFlights(string[] departureCodes, string arrivingAt, string departureDate)
+        {
+            return _flightData.GetFlights().Where(f => departureCodes.Contains(f.DepartingFrom) && f.TravalingTo == arrivingAt && f.DepartureDate >= Convert.ToDateTime(departureDate)).ToList();                            
+        }
+    }
+}
